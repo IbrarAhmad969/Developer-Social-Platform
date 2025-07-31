cefb6283f09a1310e675dcc3ef35d030a73afb57
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-07-31 16:11:42 +0500

    Add user model enhancements, JWT authentication, and Cloudinary integration; update dependencies

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -1,4 +1,6 @@
-const {mongoose, Schema} = require("mongoose")
+const { mongoose, Schema } = require("mongoose")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new Schema({
 
@@ -9,9 +11,62 @@ const userSchema = new Schema({
   role: {
     type: String,
     required: [true, "Role is Required"],
+  },
+  coverImage: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String,
+    require: [true, "Password is missing "]
   }
 
-}, {timestamps: true});
+}, { timestamps: true });
+
+//this is pre built in hook which will check if the password is yet modified or not. 
+
+userSchema.pre("save", async function (next) {
+
+  if (!this.isModified("password")) return next()
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+
+})
+// these are now methods on the model 
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(this.password, password);
+}
+
+userSchema.methods.generateAccessToken = async function(){
+  return jwt.sign(
+    {
+      _id: this.id,
+      name: this.name,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = async function(){
+  return jwt.sign(
+    {
+      _id: this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  )
+}
 
 const User = mongoose.model('User', userSchema);
 
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,9 +1,13 @@
-const express = require("express")
+const express = require("express");
 
-const {httpGetAllUsers, httpCreateUser, httpDeleteUserById, httpUpdateUserById} = require("./users.controller");
-const {createUserSchema} = require("../utils/validators/user.validator");
+const {
+  httpGetAllUsers,
+  httpCreateUser,
+  httpDeleteUserById,
+  httpUpdateUserById,
+} = require("./users.controller");
+const { createUserSchema } = require("../utils/validators/user.validator");
 const validateRequest = require("../middleware/validate");
-
 
 const routers = express.Router();
 
@@ -11,6 +15,5 @@ routers.get("/", httpGetAllUsers);
 routers.post("/", validateRequest(createUserSchema), httpCreateUser);
 routers.delete("/:id", httpDeleteUserById);
 routers.put("/:id", httpUpdateUserById);
-
 
 module.exports = routers;
